f70b4ae4fb5e525c477e3075c50aad9fc53002e2
DivyashreeChatra <86050639+DivyashreeChatra@users.noreply.github.com>
2021-06-20 10:49:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo instance = new StudentInfo();
+            instance.SetId("1000");
+            instance.SetName("Divya");
+            instance.SetMarks("70");
+            string idValue = instance.GetId();
+            string nameValue = instance.GetName();
+            string marksValue = instance.GetMarks();
+            System.Console.WriteLine(idValue);
+            System.Console.WriteLine(nameValue);
+            System.Console.WriteLine(marksValue);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ StudentInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class StudentInfo
+    {
+        //state private
+        private string id;
+        private string name;
+        private string marks;
+
+        //Behaviours,public interface/Mutators
+        //message passing(callling a method/method Invocation)
+        public string GetId()
+        {
+            return this.id;
+        }
+        public void SetId(string value)
+        {
+            this.id= value;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public void SetName(string value)
+        {
+            this.name = value;
+        }
+        public string GetMarks()
+        {
+            return this.marks;
+        }
+        public void SetMarks(string value)
+        {
+            this.marks = value;
+        }
+    }
+}
